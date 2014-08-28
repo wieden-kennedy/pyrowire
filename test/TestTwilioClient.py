4a9c60c1aa3dc903e680db9289300296da538281
Keith Hamilton <the.keith.hamilton@gmail.com>
2014-08-28 13:11:12 -0700

    added tests for custom key and no key

--- test/TestTwilioClient.py
+++ test/TestTwilioClient.py
@@ -1,7 +1,7 @@
 import unittest
 
 from pyrowire import pyrowire as pyro
-from pyrowire.messaging.sms import sms
+from pyrowire.messaging.send import sms
 from test import test_settings
 
 pyro.configure(settings=test_settings)
@@ -17,9 +17,14 @@ class TestTwilioClient(unittest.TestCase):
         success = sms(data=None, key='final_message')
         self.assertFalse(success)
 
-    def test_no_key(self):
+    def test_custom_key(self):
         job_data = {'message':'Test', 'final_message': 'TestTestTest', 'number': '+15039280913', 'topic':'sample'}
-        success = sms(data=job_data, key=None)
+        success = sms(data=job_data, key='final_message')
+        self.assertTrue(success)
+
+    def test_no_reply_or_key(self):
+        job_data = {'message':'Test', 'final_message': 'TestTestTest', 'number': '+15039280913', 'topic':'sample'}
+        success = sms(data=job_data)
         self.assertFalse(success)
 
 
