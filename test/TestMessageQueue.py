b962bf25e665ca7abee7d10a15b812e65c2b6d67
Keith Hamilton <the.keith.hamilton@gmail.com>
2014-08-28 05:44:45 -0700

    removed leading underscores; added test for Twilio max length

--- test/TestMessageQueue.py
+++ test/TestMessageQueue.py
@@ -1,3 +1,5 @@
+import random
+import string
 import unittest
 
 from redis import Redis
@@ -22,56 +24,65 @@ class TestMessageQueue(unittest.TestCase):
         self.redis.delete('%s.completed' % self.topic)
 
     def test_profane_message(self):
-        _expected_response = config.validators(self.topic)['profanity']
-        _message = 'fuck'
-        _response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, _message), follow_redirects=True)
+        expected_response = config.validators(self.topic)['profanity']
+        message = 'fuck'
+        response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, message), follow_redirects=True)
 
-        _data = str(_response.data).split('<Body>')[1].split('</Body>')[0]
+        data = str(response.data).split('<Body>')[1].split('</Body>')[0]
 
-        self.assertEqual(_response.status, "200 OK")
-        self.assertEqual(_data, _expected_response)
+        self.assertEqual(response.status, "200 OK")
+        self.assertEqual(data, expected_response)
 
     def test_message_too_long(self):
-        _expected_response = config.validators(self.topic)['length']
-        _message = 'bkndlznbezfmuktjdsvosezchjlgvjznblxvoihpdasfxifpiupzlzrphfswozdjbgiohemtbyuuoqzvbxiixhkyryheizoqxnp' \
-                'tutihnohujythamvptubsrdjlgxqfmltvdfvksbmpddsdfasdfasdfasdweljweoibtgsgdlicwwnmgeduvrezso'
-        _response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, _message), follow_redirects=True)
+        message = ''.join(random.choice(string.ascii_letters) for i in range(161))
+        expected_response = config.validators(self.topic)['length']
+        response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, message), follow_redirects=True)
 
-        _data = str(_response.data).split('<Body>')[1].split('</Body>')[0]
+        data = str(response.data).split('<Body>')[1].split('</Body>')[0]
 
-        self.assertEqual(_response.status, "200 OK")
-        self.assertEqual(_data, _expected_response)
+        self.assertEqual(response.status, "200 OK")
+        self.assertEqual(data, expected_response)
+        
+    def test_message_exceeds_twilio_maximum_length(self):
+        message = ''.join(random.choice(string.ascii_letters) for i in range(1600))
+        expected_response = config.validators(self.topic)['length']
+        response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, message), follow_redirects=True)
+
+        data = str(response.data).split('<Body>')[1].split('</Body>')[0]
+
+        self.assertEqual(response.status, "200 OK")
+        self.assertEqual(data, expected_response)
 
     def test_message_zero_length(self):
-        _expected_response = config.validators(self.topic)['length']
-        _message = ''
-        _response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, _message), follow_redirects=True)
+        expected_response = config.validators(self.topic)['length']
+        message = ''
+        response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, message), follow_redirects=True)
 
-        _data = str(_response.data).split('<Body>')[1].split('</Body>')[0]
+        data = str(response.data).split('<Body>')[1].split('</Body>')[0]
 
-        self.assertEqual(_response.status, "200 OK")
-        self.assertEqual(_data, _expected_response)
+        self.assertEqual(response.status, "200 OK")
+        self.assertEqual(data, expected_response)
 
     def test_message_not_parseable(self):
-        _expected_response = config.validators(self.topic)['parseable']
-        _message = '%F0%9F%98%AC'
-        _response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, _message), follow_redirects=True)
+        expected_response = config.validators(self.topic)['parseable']
+        message = '%F0%9F%98%AC'
+        response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, message), follow_redirects=True)
 
-        _data = str(_response.data).split('<Body>')[1].split('</Body>')[0]
+        data = str(response.data).split('<Body>')[1].split('</Body>')[0]
 
-        self.assertEqual(_response.status, "200 OK")
-        self.assertEqual(_data, _expected_response)
+        self.assertEqual(response.status, "200 OK")
+        self.assertEqual(data, expected_response)
 
     def test_good_message(self):
-        _messages = ['Frank', 'Joe Bob', 'Mary Kate ']
-        _expected_response = config.accept_response(self.topic)
-        for n in _messages:
-            _response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, n), follow_redirects=True)
+        messages = ['Frank', 'Joe Bob', 'Mary Kate ']
+        expected_response = config.accept_response(self.topic)
+        for n in messages:
+            response = self.test_app.get('/queue/%s?Body=%s&From=+1234567890' % (self.topic, n), follow_redirects=True)
 
-            _data = str(_response.data).split('<Body>')[1].split('</Body>')[0]
+            data = str(response.data).split('<Body>')[1].split('</Body>')[0]
 
-            self.assertEqual(_response.status, "200 OK")
-            self.assertEqual(_data, _expected_response)
+            self.assertEqual(response.status, "200 OK")
+            self.assertEqual(data, expected_response)
 
 if __name__ == '__main__':
     unittest.main()
