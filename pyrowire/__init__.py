1b3273665646728b4eaf8d17c6c6db5fc91a6fd3
Keith Hamilton <the.keith.hamilton@gmail.com>
2014-08-28 09:47:53 -0700

    refactor

--- pyrowire/__init__.py
+++ pyrowire/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'keith.hamilton'
 from pyrowire import configure
 from decorators.decorators import handler, validator
-from messaging.sms import sms
+from messaging.send import sms
 from messaging.message import message_from_request
 from runner.runner import run
