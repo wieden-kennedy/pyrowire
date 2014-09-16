a95cbd57437002e51b319039bc704a6c114c0b1a
Keith Hamilton <the.keith.hamilton@gmail.com>
2014-09-16 10:49:11 -0700

    added heroku setup stuff, added parser options

--- pyrowire/resources/console_scripts/console_scripts.py
+++ pyrowire/resources/console_scripts/console_scripts.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
+import argparse
 import os
 import re
 import shutil
+import sys
+
+from fabric.api import *
+from fabric.colors import red as _red, green as _green, yellow as _yellow, white as _white
+from fabric.utils import abort
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="command line args to assist with development")
+    parser.add_argument('--init', dest='init', default=None, help='initialize stub files for pyrowire development')
+    parser.add_argument('--deploy-heroku', dest='deploy_heroku', default=None,
+                        help='deploy current pyrowire project to heroku')
+    return parser.parse_args()
 
 
 def init():
@@ -26,5 +40,143 @@ def init():
     \n
     ''' + '\n\t'.join('* ./%s' % x for x in copied_files) + \
           'For help on how to get started, check out the README at https://github.com/wieden-kennedy/pyrowire/'
+
+
+def install_heroku_toolbelt():
+    is_installed = False
+    install = prompt(_white("Heroku Toolbelt is not installed. Would you like to install it? [n] ")) or None
+    if install:
+        if re.search('darwin', sys.platform):
+            # install with brew if available
+            brew = local('which brew', capture=True)
+            if brew:
+                local('brew install heroku-toolbelt')
+                is_installed = True
+            else:
+                abort(_red("Attempted to install heroku-toolbelt with brew, but brew was not found. "
+                           "Please install homebrew and try again, or install the heroku-toolbelt manually "
+                           "from https://toolbelt.heroku.com"))
+
+        elif re.search('linux', sys.platform):
+            # install with wget if ubuntu
+            is_ubuntu = local('uname -v', capture=True)
+            if re.search('ubuntu', is_ubuntu.lower()):
+                sudo('wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh')
+                is_installed = True
+
+    if is_installed:
+        print(_green("Heroku Toolbelt successfully installed"))
+
+
+def heroku_remote_exists():
+    remotes = local('git remote -v', capture=True).split('\n')
+    return len([i for i in remotes if re.search('heroku', i)]) > 0
+
+
+def heroku_select_account():
+    # select heroku account
+    accounts = local('heroku accounts', capture=True) or None
+    if accounts:
+        return prompt(_white("Type the name of the Heroku account you would like to use:\n\t%s%s\n" %
+                             (_yellow('* '), _yellow('\n\t* '.join(accounts.split('\n'))))))
+
+    else:
+        add_account = prompt(_white("No heroku toolbelt account was found. Would you like to add one? [n] ")) or None
+        if add_account:
+            account_name = prompt(_white("Type the name of the account you would like to add"))
+            local('heroku accounts:add %s' % account_name)
+            return account_name
+
+
+def heroku_select_or_create_app(account_name):
+    account_applications = local('heroku apps --account %s' % account_name, capture=True)
+    app = prompt(_white("Type the name of the application you wish to use, or press "
+                        "Enter to create a new one:\n\t%s%s\nSelection: " %
+                        (_yellow('* '), _yellow('\n\t* '.join(account_applications.split('\n')))))) or None
+    if app:
+        if app in account_applications:
+            return app
+
+    app = prompt(_white("Type the name of the app you would like to create: "))
+    local('heroku apps:create %s --account %s' % (app, account_name))
+    return app
+
+
+def heroku_add_redis(app, account):
+    redis_host = 'barreleye.redistogo.com'
+    redis_port = 6379
+    redis_password = None
+    redis_db = 0
+
+    local('heroku addons:add --app %s --account %s redistogo:nano' % (app, account))
+    open_redis_details = prompt(_white("Would you like to open the addon's details in your browser now? [n] ")) or False
+    if open_redis_details:
+        prompted = prompt(_yellow("Once the redis details open, you will be prompted to enter some details. "
+                                  "You can type 'q' at any time to cancel updating your application's Redis details."
+                                  "Press Enter to open the Redis details page."))
+        if prompted != 'q':
+            local('heroku addons:open --app %s --account %s redistogo:nano') % (app, account)
+            prompted = prompt(_white("Paste or type the Redis port number: "))
+        if prompted != 'q':
+            redis_port = int(prompted)
+            prompted = prompt(_white("Paste or type the Redis password: "))
+        if prompted != 'q':
+            redis_password = prompted
+
+    is_staging = prompt(_white("Which environment is this for?\n\t1. Staging\n\t2. Production\nSelection: "))
+    profile = 'staging'
+    if '2' in is_staging:
+        profile = 'prod'
+    settings_file = [i for i in os.listdir('../') if re.search('settings', i) and not re.search(r'.*\.pyc$', i)][0]
+    settings_path = os.path.join('../', settings_file)
+
+    f = open(settings_path, 'r')
+    settings_lines = [
+        i.replace('<%s.redis_host>' % profile, redis_host)
+            .replace('<%s.redis_port>' % profile, str(redis_port))
+            .replace('<%s.redis_password>' % profile, redis_password) for i in f.readlines()]
+
+    f = open('%s' % settings_path, 'w')
+    for line in settings_lines:
+        f.write(line)
+    f.close()
+
+
+def heroku_deploy():
+    if not heroku_remote_exists():
+        # check if heroku toolbelt is available
+        heroku_bin = local('which heroku', capture=True)
+        if not heroku_bin:
+            install_heroku_toolbelt()
+
+        # log in to heroku
+        selected_account = heroku_select_account()
+        local('heroku login --account %s' % selected_account)
+
+        # select or create an app
+        app_name = heroku_select_or_create_app(selected_account)
+
+        # add redis ?
+        add_redis = prompt(_white("Type 'y' to add Redis To Go to your Heroku application "
+                                  "(to use an external service, update your settings file): ")) or None
+        if add_redis.lower() == 'y':
+            heroku_add_redis(app_name, selected_account)
+        # add git remote
+        local('git remote add heroku heroku.com:%s.git' % app_name)
+
+        print(_green("You are ready to commit any changes and deploy to heroku by typing: %s" %
+                     _white("git push heroku <name_of_branch>")))
+        return None
+    print(_green("Looks like there is already a heroku remote set up. Huzzah!"))
+
+
+def main():
+    args = parse_args()
+    if args.init:
+        init()
+    elif args.deploy_heroku:
+        heroku_deploy()
+
+
 
 
